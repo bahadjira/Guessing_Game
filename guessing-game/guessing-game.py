28140ec1402b44f2fd8f7f5e94ecf5909122bae2
Hadjiratou <bahadjira@gmail.com>
2020-07-06 02:22:11 -0400

    These are the changes

--- guessing-game/guessing-game.py
+++ guessing-game/guessing-game.py
@@ -1,19 +1,31 @@
+from flask import Flask, render_template, request
 import random
-number = random.randint(1,10)
-name = input("Enter your name: ")
-number_guess = 0
-print(name + ' Guess a number between 1,10: ' )
-while number_guess < 5:
-    guess = int(input())
-    number_guess += 1
-    if guess < number:
-        print('Your guess is too low')
-    if guess > number:
-        print('Your guess is too high')
-    if guess == number:
-        break
-if guess == number:
-    print('You guessed the number in ' + str(number_guess) + ' tries!')
-else:
-    print('You did not guess the number, the number was ' + str(number))
-print("done")
+
+app = Flask(__name__)
+global comp_num, counter
+counter = 0
+comp_num = random.randit (1,10)
+wsgi_app = app.wsgi_app
+
+@app.route('/', methods = ['GET', 'POST'])
+def Guess_Game():
+    global comp_num, counter
+    message = ""
+
+    if request.method == 'POST'
+        counter += 1
+        form = request.form
+        user_guess = int (form["guess"])
+        if comp_num == user_guess
+            message = "well done"
+            return render_template("game_over.html")
+        elif comp_num > user guess:
+            message = "Too low"
+        else:
+            message = "too high"
+        if counter == 3: 
+            message = "you failed"
+            return render_template("game_over.html", message = message)
+    return render_template ("game.html" , message = message)
+
+            
